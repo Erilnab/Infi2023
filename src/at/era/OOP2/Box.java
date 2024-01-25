5e2124d511937578436acbf3484edeabc79d439c
elira <elirab973@gmail.com>
2024-01-25 15:14:49 +0100

    OOP F1Auto

--- src/at/era/OOP2/Box.java
+++ src/at/era/OOP2/Box.java
@@ -8,4 +8,37 @@ public class Box {
     private int boss;
     private ArrayList<F1Auto> f1AutoList;
 
+    public Box
+
+    public int getFlaeche() {
+        return flaeche;
+    }
+
+    public void setFlaeche(int flaeche) {
+        this.flaeche = flaeche;
+    }
+
+    public int getGled() {
+        return gled;
+    }
+
+    public void setGled(int gled) {
+        this.gled = gled;
+    }
+
+    public int getBoss() {
+        return boss;
+    }
+
+    public void setBoss(int boss) {
+        this.boss = boss;
+    }
+
+    public ArrayList<F1Auto> getF1AutoList() {
+        return f1AutoList;
+    }
+
+    public void setF1AutoList(ArrayList<F1Auto> f1AutoList) {
+        this.f1AutoList = f1AutoList;
+    }
 }
